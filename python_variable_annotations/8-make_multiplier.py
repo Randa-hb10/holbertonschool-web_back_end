2146d9c44b7a24c7bb66d94ca07107ea615c0d4a
Randa-hb10 <rm-400000@hotmail.com>
2026-04-13 10:13:03 +0300

    add task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module provides a function that returns a multiplier function."""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier."""
+    
+    def multiply(x: float) -> float:
+        """Multiplies x by multiplier."""
+        return x * multiplier
+
+    return multiply
